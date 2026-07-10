92703ac747e1f013f9ae503f8999b78383b324f0
Andres Felipe Granada Peñaloza <38518871@claro.com.co>
2026-07-10 14:49:59 -0500

    Fix CORS: allow_methods y allow_headers no eran URLs

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://candid-mochi-ccd7c4.netlify.app/"],
-    allow_methods=["https://candid-mochi-ccd7c4.netlify.app/"],
-    allow_headers=["https://candid-mochi-ccd7c4.netlify.app/"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 def serialize(doc):
